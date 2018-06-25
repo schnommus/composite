65772b66f45e71874b204184467c6789b7a4f6d8
Sebastian Holzapfel <seb.holzapfel@data61.csiro.au>
2018-06-25 21:44:16 +1000

    SDL rendering and correct VSYNC

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
 extern crate byteorder;
+extern crate sdl2;
 
 use std::fs::File;
 use std::env;
 use std::process;
 
 use byteorder::*;
+
+use sdl2::pixels;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+
+const SCREEN_WIDTH: u32 = 800;
+const SCREEN_HEIGHT: u32 = 600;
 
 const BUF_SZ:               usize = 1024;
 const HALT_AT:              usize = 4096*1600;
@@ -21,6 +29,7 @@ const H_LINE_SAMPLES:       usize = (SAMPLE_RATE * H_ACTIVE_VIDEO_SEC) as usize;
 const V_SYNC_SECTION_SEC:     f32 = H_LINE_TIME_SEC/2.0;
 const V_SHORT_SYNC_PULSE_SEC: f32 = 2.35e-6;
 const V_BROAD_SYNC_PULSE_SEC: f32 = 4.7e-6;
+const V_BLANKING_LINES:     usize = 25;
 
 const SYNC_THRESHOLD:         f32 = 0.07;
 const SYNC_LEN_DELTA:         f32 = 0.5e-6;
@@ -30,9 +39,11 @@ const EVEN_FRAME_HI_SEC:      f32 = 27.35e-6;
 const ODD_FRAME_HI_SEC:       f32 = 59.35e-6;
 
 // Indices relative to the END of an HSYNC pulse
-const SCANLINE_START_N:     usize = (SAMPLE_RATE * H_FRONT_PORCH_SEC) as usize;
+const SCANLINE_START_N:     usize = (SAMPLE_RATE * H_BACK_PORCH_SEC) as usize;
 const SCANLINE_END_N:       usize = (SAMPLE_RATE *
-                                     (H_FRONT_PORCH_SEC + H_ACTIVE_VIDEO_SEC)) as usize;
+                                     (H_BACK_PORCH_SEC + H_ACTIVE_VIDEO_SEC)) as usize;
+
+type Canvas = sdl2::render::Canvas<sdl2::video::Window>;
 
 #[derive(Debug, PartialEq)]
 enum SyncPulse {
@@ -60,10 +71,13 @@ struct CompositeDecode {
     last_field: Field,
 
     cur_scanline: [f32; H_LINE_SAMPLES],
+    cur_scanline_index: i32,
+
+    canvas: Canvas,
 }
 
 impl CompositeDecode {
-    fn new() -> CompositeDecode {
+    fn new(canvas: Canvas) -> CompositeDecode {
         let input_buffer = [0.0; BUF_SZ];
         let input_index = 0;
         let in_sync_pulse = false;
@@ -72,6 +86,7 @@ impl CompositeDecode {
         let last_flat_sec = 0.0;
         let last_sync = SyncPulse::Unknown;
         let last_field = Field::Unknown;
+        let cur_scanline_index = 0;
         CompositeDecode {
             input_buffer,
             input_index,
@@ -81,6 +96,8 @@ impl CompositeDecode {
             last_sync,
             last_field,
             cur_scanline,
+            cur_scanline_index,
+            canvas,
         }
     }
 
@@ -111,6 +128,39 @@ impl CompositeDecode {
                     .copy_from_slice(&values[values.len() - n_left..]);
                 self.input_index += n_left;
             }
+        }
+
+    }
+
+    fn draw_scanline(&mut self) {
+        if self.last_field == Field::Unknown ||
+           self.last_sync == SyncPulse::Unknown {
+               return;
+        }
+
+        if self.last_sync == SyncPulse::VerticalShort  {
+            self.cur_scanline_index = match self.last_field {
+                Field::Even => -1 * V_BLANKING_LINES as i32 - 1,
+                Field::Odd => -1 * V_BLANKING_LINES as i32,
+                _ => 0,
+            };
+            return;
+        }
+
+        if self.last_sync == SyncPulse::Horizontal &&
+           self.cur_scanline_index >= 0 &&
+           self.cur_scanline_index < (SCREEN_HEIGHT as i32) {
+                for x in 0..(SCREEN_WIDTH-1) {
+                    let x_csl = ((x * H_LINE_SAMPLES as u32) / SCREEN_WIDTH) as usize;
+                    let v = (200.0 * (self.cur_scanline[x_csl] - 0.1)) as u8;
+                    self.canvas.set_draw_color(pixels::Color::RGBA(v, v, v, 255));
+                    self.canvas.draw_point((x as i32, self.cur_scanline_index));
+                }
+            self.canvas.present();
+        }
+
+        if self.last_sync == SyncPulse::Horizontal {
+            self.cur_scanline_index += 2;
         }
 
     }
@@ -150,9 +200,10 @@ impl CompositeDecode {
 
                 let len_sec = self.since_edge as f32 * (1./SAMPLE_RATE);
 
-
                 // Just came out of a sync pulse - record its length
                 if self.in_sync_pulse == false {
+
+                    self.draw_scanline();
 
                     print!("sync: {:.2} usec [{} samples] \t",
                            len_sec * 1e6,
@@ -191,6 +242,27 @@ impl CompositeDecode {
     }
 }
 
+fn init_sdl2() -> (Canvas, sdl2::Sdl) {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsys = sdl_context.video().unwrap();
+    let window =
+        video_subsys.window("composite-rapid-sdl2",
+                            SCREEN_WIDTH,
+                            SCREEN_HEIGHT)
+                    .position_centered()
+                    .opengl()
+                    .build()
+                    .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(pixels::Color::RGB(0, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    (canvas, sdl_context)
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
@@ -201,7 +273,9 @@ fn main() {
 
     let mut file = File::open(args[1].clone()).unwrap();
 
-    let mut decoder = CompositeDecode::new();
+    let (mut canvas, mut context) = init_sdl2();
+
+    let mut decoder = CompositeDecode::new(canvas);
 
     let mut halt_count = 0;
 
@@ -211,6 +285,21 @@ fn main() {
         halt_count += 1;
         if halt_count > HALT_AT {
             break;
+        }
+    }
+
+    let mut events = context.event_pump().unwrap();
+    'main: loop {
+        for event in events.poll_iter() {
+            match event {
+                Event::Quit {..} => break 'main,
+                Event::KeyDown {keycode: Some(keycode), ..} => {
+                    if keycode == Keycode::Escape {
+                        break 'main
+                    }
+                }
+                _ => {}
+            }
         }
     }
 }
